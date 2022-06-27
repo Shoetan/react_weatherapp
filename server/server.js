4b09fb24db89f7bcfc6c28adf1c4bbbb34a5426c
unknown <emmysoetan@gmail.com>
2022-06-27 22:07:36 +0100

    Created a mini node express backend to pull data from the open weather api

--- server/server.js
+++ server/server.js
@@ -1,56 +1,76 @@
 const express = require('express');
 const cors = require('cors');
-const { default: axios } = require('axios');
-const { response } = require('express');
-
+const axios = require('axios');
 const app = express();
+const PORT = 5001
+// connfiguration for the api to use api to use.
 
 app.use(cors());
 
+app.use(express.json())
+
 require('dotenv').config()
 
-const port = 5001
-
-
-const apiConfig = {
-    method: 'GET',
-    url: 'https://community-open-weather-map.p.rapidapi.com/weather',
-    params: {
-      q: 'London,uk',
-      lat: '0',
-      lon: '0',
-      callback: 'test',
-      id: '2172797',
-      lang: 'null',
-      units: 'imperial',
-      mode: 'xml'
-    },
-    headers: {
-      'X-RapidAPI-Key': '01b6f99954msh2b2ae87b6375022p1c2376jsnaa8a0702eb72',
-      'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
-    }
-  };
-
-const getForecast = async ()=>{
-    try {
-        const data = await axios.request (apiConfig)
-        console.log(data.data.value);
-    } catch (error) {
-        console.log(error.message);
-    }
-}
-
-getForecast()
 
 
 
 
 
+
+
+let data
+// Function to get forecast details from the openweatherapi.
 
 
 app.get('/', (req,res) =>{
- 
+
+  const getForecast = async ()=> {
+
+    const location = req.body.city
+   
+    // connfiguration for the api to use api to use.
+    const apiConfig = {
+      method: 'GET',
+      url: 'https://community-open-weather-map.p.rapidapi.com/weather', 
+      
+      params : {
+        q: 'london, uk',
+        lat: '0',
+        lon: '0',
+        callback: 'test',
+        id: '2172797',
+        lang: 'null',
+        units: 'imperial',
+        mode: 'xml'    
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.API_KEY,
+        'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com'
+      }
+  
+      }
+      try {
+          const response = await axios.request (apiConfig)
+          data = response.data
+          console.log(data);
+      } catch (error) {
+          console.log(error.message);
+      }
+  }
+  
+  getForecast()
+
+
+
+
+
+  res.send(data)
 })
-app.listen(port, ()=>{
-    console.log(`Server running at http://localhost:${port}`)
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running at http://localhost:${PORT}`)
 });
