35d10d10e22fe8476e6eda79b4f45fca8125bd19
unknown <emmysoetan@gmail.com>
2022-06-30 13:25:34 +0100

    added a custom font of poppins to the tailwind config file also finshed the input field to take in the user city

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
-import { useEffect, useState } from 'react'
 import axios from 'axios'
 
+import { useState } from 'react'
 
 const App = () => {
 
-  const [city, setCity] = useState('')
+const [ city, setCity] = useState("")
 
-  const getForecast = async () => {
+const getForecast = async () => {
+
+    const config ={
+      method : 'GET',
+      url : 'http://localhost:5001/',
+      params : {
+         userCity : city
+      }
+    }
 
     try {
-      const res =  await axios.get('http://localhost:5001/')
+      const res =  await axios.request(config)
       console.log(res);
     } catch (error) {
       console.log(error.message);
@@ -17,16 +25,14 @@ const App = () => {
 
   }
 
-
-
-
   return ( 
     <>
-    <div>
-      <input type="text" onChange={(e) => { setCity(e.target.value); } } />
+    <div className='flex gap-4 w-1/2 m-auto justify-around mt-4'>
+      <input className = 'w-4/6 bg-transparent border-b-2 border-teal-50 focus:outline-none' placeholdertype="text" onChange={(e) => { setCity(e.target.value); } } />
       {city}
+      <button className='font-poppins bg-teal-50 p-4 rounded-lg hover:bg-teal-100' onClick={getForecast}>Get Forecast</button>
     </div>
-    <button onClick={getForecast}>Get Forecast</button>
+   
     </>
    );
 }
